9e70c4c2c381c5b4d1a7c8a6ed74e28dc4f66426
vkraussl <thekrauss31@gmail.com>
2024-10-08 14:10:06 +0200

    Correction de l'affichage des données JSON avec `Debug`, gestion des intervalles de dates et sauvegarde dans un fichier

--- src/api_coin_service.rs
+++ src/api_coin_service.rs
@@ -43,35 +43,3 @@ pub async fn api_coin_exchange_rates(assets: &str, start: &str, end: &str) -> Re
         )))
     }
 }
-
-/* 
-pub async fn api_coin_service() -> Result<(), Box<dyn Error>> {
-    let api_key = get_api_key();
-    let client = Client::new();
-
-    let res = client
-        .get(BASE_URL)
-        .header("X-CoinAPI-Key", api_key)
-        .send()
-        .await?;
-
-        if res.status() == StatusCode::OK {
-            println!("The API call worked. Status: {}", res.status());
-           // println!("Headers:\n{:#?}", res.headers());
-    
-            let body = res.text().await?;
-            let json_data:Value = serde_json::from_str(&body)?; // déserialisation du json
-    
-            if let Some(array) = json_data.as_array() {
-                let frist_10 = &array[..std::cmp::min(10, array.len())];
-                println!("Frist 10 elements: {:#?}", frist_10);
-            } else {
-                println!("The response is not an array.");
-            }
-        } else {
-            println!("API call failed with status: {}", res.status());
-        }
-
-    Ok(())
-}
-*/
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,113 @@
-use chrono::{Duration, Utc};
-use api_coin_service::api_coin_exchange_rates;
-use serde_json::json;
+use chrono::{NaiveDate, Duration, Utc};
+use serde::{Deserialize, Serialize};
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
+use std::path::Path;
+use crate::api_coin_service::api_coin_exchange_rates;
+
 
 mod api_config;
 mod api_coin_service;
 
-// fonction pour sauvegarder les données JSON dans un fichier
-fn save_json_data_to_file(filename: &str, json_data: &str) -> std::io::Result<()> {
-    let mut file = File::create(filename)?;
-    file.write_all(json_data.as_bytes())?;
-    Ok(())
+#[derive(Debug, Serialize, Deserialize)]
+struct Rate {
+    date: String,
+    value: f64, 
 }
+
+
 
 // fonction pour formater les taux de change en JSON
-fn get_json_rates(rates_data: &Vec<api_coin_service::ExchangeRate>) -> String{
-    let rates_json: Vec<_> = rates_data.iter().map(|r|{
-        json!({
-            "date": r.time_period_start[..10].to_string(),
-            "value": r.rate_close
-        })
-    }).collect();
-
-    serde_json::to_string(&rates_json).unwrap()
+fn get_json_rates(rates_data: &Vec<Rate>) -> String {
+    serde_json::to_string(&rates_data).unwrap()
 }
 
+
+// charger des données JSON à partir d'un fichier
+fn load_json_data_from_file(filename: &str) -> Vec<Rate> {
+    let mut file = File::open(filename).expect("File no found");
+    let mut json_data = String::new();
+    file.read_to_string(&mut json_data).expect("Error reading file");
+    serde_json::from_str(&json_data).expect("Error while deserializing JSON")
+}
+
+    // sauvegarder les données JSON dans un fichier
+fn save_json_data_to_file(filename: &str, json_data: &str) {
+    let mut file = File::create(filename).expect("Impossible de créer le fichier");
+    file.write_all(json_data.as_bytes()).expect("Erreur lors de l'écriture du fichier");
+}
+
+fn get_dates_intervals(date_start: NaiveDate, date_end: NaiveDate, max_days: i64) -> Vec<(NaiveDate, NaiveDate)> {
+    let mut intervals = Vec::new();
+    let mut current_start = date_start;
+    let mut diff_date = (date_end - date_start).num_days();
+
+    while diff_date > 0 {
+        let interval_length = std::cmp::min(max_days - 1, diff_date);
+        let current_end = current_start + Duration::days(interval_length);
+        intervals.push((current_start, current_end));
+        current_start = current_end + Duration::days(1);
+        diff_date -= interval_length + 1
+    }
+    intervals
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // calcule la date d'aujourd'hui et la date de début (10 jours avant)
-    let date_today = Utc::today();
-    let date_start = date_today - Duration::days(10);
+    let date_end: chrono::DateTime<Utc> = Utc::now();
+    let date_end_str = date_end.format("%Y-%m-%d").to_string();
 
-    let date_today_str = date_today.format("%Y-%m-%d").to_string();
+
+    let date_start = date_end - Duration::days(10);
     let date_start_str = date_start.format("%Y-%m-%d").to_string();
 
-    println!("Start day: {}", date_start_str);
-    println!("End day: {}", date_today_str);
 
         // défini les actifs pour l'API
     let assets = "BTC/EUR";
-    
-    // appel de l'API pour récupérer les taux de change
-    let rates = api_coin_exchange_rates(&assets, &date_start_str, &date_today_str).await?;
-    
-    // vérification si des taux de change ont été reçus
-    if !rates.is_empty(){
+    let data_filename = assets.replace("/", "_") + ".json";
 
-        let json = get_json_rates(&rates);
-
-        let filename = assets.replace("/", "_") + ".json";
-
-        save_json_data_to_file(&filename, &json);
-
-        println!("{:}", json);
-        println!("{}, nombre de cours: {}", assets, rates.len());
-        
-        for rate in &rates {
-            println!("{} : {}", &rate.time_period_start[..10], rate.rate_close);
-        }
-    } else {
-        println!("No exchange rate received.")
+        // Charger les données JSON depuis le fichier si le fichier existe
+    let mut rates : Vec<Rate> = Vec::new();
+    if Path::new(&data_filename).exists(){
+        rates = load_json_data_from_file(&data_filename);
     }
+        // Si des données sont déjà présentes dans le fichier
+    if !rates.is_empty(){
+        let saved_data_date_start_str = &rates[0].date;
+        let saved_data_date_end_str = &rates[rates.len() -1].date;
+
+        println!("Saved data : start {}, end {}", saved_data_date_start_str, saved_data_date_end_str);
+    } else {
+        println!("No saved data found.");
+    }
+
+
+     // Conversion des chaînes en NaiveDate avec parse_from_str
+    let date_start = NaiveDate::parse_from_str(&date_start_str, "%Y-%m-%d").expect("Date start invalid");
+    let date_end = NaiveDate::parse_from_str(&date_end_str, "%Y-%m-%d").expect("Date end invalid");
+
+        // Appel API avec intervalles
+        let intervals = get_dates_intervals(date_start, date_end, 100);
+        for (start, end) in intervals  {
+        println!("Intervals: {} - {}", start, end);
+
+        let rates_data = api_coin_exchange_rates(assets, &start.to_string(), &end.to_string()).await?;
+        for rate in rates_data{
+            rates.push(Rate { 
+                date: rate.time_period_start[..10].to_string(),
+                value: rate.rate_close,
+            });
+        }
+    }
+
+    let json_data =  get_json_rates(&rates);
+    save_json_data_to_file(&data_filename, &json_data);
     
+    println!("{:?}", rates);
+    println!("Data saved in {}", data_filename);
+
+
+        
     Ok(())
 }
